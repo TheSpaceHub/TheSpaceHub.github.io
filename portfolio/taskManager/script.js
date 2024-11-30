834b3ec2639a6d408faf6dc2d3be22d5b7ca70e4
TheSpaceHub <danvancea1235813@gmail.com>
2024-11-30 12:46:15 +0100

    Rebuild site properly

--- portfolio/taskManager/script.js
+++ portfolio/taskManager/script.js
@@ -15,15 +15,15 @@ function HTMLTaskToDo(task)
 {
     return "<div class='task'>\n<h3>" + task.task
     + "</h3>\n<div class='buttons'>\n<button class='complete' id='complete"
-    + task.id +  "'>Complete</button>\n<button class='delete' id='delete"
-    + task.id + "'>Delete</button>\n</div>\n</div>\n";
+    + task.id +  "'>COMPLETE</button>\n<button class='delete' id='delete"
+    + task.id + "'>DELETE</button>\n</div>\n</div>\n";
 }
 
 function HTMLCompletedTask(task)
 {
     return "<div class='task'>\n<h3>" + task.task
     + "</h3>\n<div class='buttons'>\n<button class='delete' id='delete"
-    + task.id + "'>Delete</button>\n</div>\n</div>\n";
+    + task.id + "'>DELETE</button>\n</div>\n</div>\n";
 }
 
 function RefreshTasksToDo()
@@ -32,7 +32,7 @@ function RefreshTasksToDo()
     for(let i = 0; i < tasksToDo.length; i++) finalHTML += HTMLTaskToDo(tasksToDo[i]);
     document.getElementById("listToDo").innerHTML = finalHTML;
     if(tasksToDo.length == 0) document.getElementById("listToDo").innerHTML = 
-    '<div class="task">\n<h3>Hurray! You have no tasks left.</h3>\n</div>';
+    '<div class="task">\n<h3>No tasks left</h3>\n</div>';
 
     for(let i = 0; i < tasksToDo.length; i++)
     {
@@ -49,7 +49,7 @@ function RefreshCompletedTasks()
     for(let i = 0; i < completedTasks.length; i++) finalHTML += HTMLCompletedTask(completedTasks[i]);
     document.getElementById("completedList").innerHTML = finalHTML;
     if(completedTasks.length == 0) document.getElementById("completedList").innerHTML = 
-    '<div class="task">\n<h3>There are no completed tasks.</h3>\n</div>';
+    '<div class="task">\n<h3>No completed tasks</h3>\n</div>';
     for(let i = 0; i < completedTasks.length; i++)
     {
         document.getElementById("delete" + completedTasks[i].id.toString()).onclick =
